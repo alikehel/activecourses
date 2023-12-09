a811bf1f0514bff08343f8d18586b075a2db85fc
Hussein Hany <hhany3997@gmail.com>
2023-12-09 18:25:04 +0200

    Responsive design updates: Adjusted image and text orientation for smaller devices in ContributorCard component

--- src/components/ContributorCard.tsx
+++ src/components/ContributorCard.tsx
@@ -2,60 +2,65 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Card = styled.div`
-    border: 1px solid #444;
-    border-radius: 8px;
-    overflow: hidden;
-    margin-bottom: 10px;
-    margin-right: 10px;
-    box-sizing: border-box;
-    width: 100%; // Full width on small screens
-    max-width: 49%; // Half width on larger screens
-    background: #222;
-    color: #fff;
+  display: inline-flex;
+  border: 1px solid #444;
+  border-radius: 8px;
+  overflow: hidden;
+  margin-bottom: 10px;
+  margin-right: 10px;
+  box-sizing: border-box;
+  width: 49%;
+  background: #222;
+  color: #fff;
 
-    @media (min-width: 600px) {
-        display: flex;
-        flex-direction: row;
-        width: 49%;
-    }
+  @media (max-width: 700px) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
 `;
 
 const ImageContainer = styled.div`
-    flex: 0 0 auto;
-    overflow: hidden;
-    width: 100%;
-    height: auto;
+  flex: 0 0 120px;
+  overflow: hidden;
+  width: 150px;
+  height: 150px;
 
-    @media (min-width: 600px) {
-        width: 150px;
-        height: 150px;
-    }
+  @media (max-width: 700px) {
+    width: 100px;
+    height: auto;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+`;
+
+const TextContainer = styled.div`
+  flex: 1;
+  padding: 10px;
 `;
 
 const ContributorCard = ({ name, role, description, imageSrc }) => (
-    <Card>
-        <ImageContainer>
-            <img
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                    objectPosition: 'center',
-                }}
-                src={
-                    imageSrc ||
-                    'https://www.pngkey.com/png/detail/950-9501315_katie-notopoulos-katienotopoulos-i-write-about-tech-user.png'
-                }
-                alt={`Contributor Image of ${name}`}
-            />
-        </ImageContainer>
-        <div style={{ flex: 1, padding: '10px' }}>
-            <div style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{name}</div>
-            <div style={{ color: '#888' }}>{role}</div>
-            {description && <p style={{ lineHeight: '1.4' }}>{description}</p>}
-            {/* Add more details as needed */}
-        </div>
-    </Card>
+  <Card>
+    <ImageContainer>
+      <Image
+        src={
+          imageSrc ||
+          'https://www.pngkey.com/png/detail/950-9501315_katie-notopoulos-katienotopoulos-i-write-about-tech-user.png'
+        }
+        alt={`Contributor Image of ${name}`}
+      />
+    </ImageContainer>
+    <TextContainer>
+      <div style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{name}</div>
+      <div style={{ color: '#888' }}>{role}</div>
+      {description && <p style={{ lineHeight: '1.4' }}>{description}</p>}
+    </TextContainer>
+  </Card>
 );
 
 export default ContributorCard;
